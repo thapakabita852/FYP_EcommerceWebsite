6d43b46ba2229fe4ed881d6efe24e006e4d222c6
thapaKabita852 <thapakabita852@gmail.com>
2025-04-13 01:06:09 +0545

    make some changes in design

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('about-us/', views.about_us_view, name='about_us'),
     path('search/', views.search_results, name='search_results'),
     path('search/suggestions/', views.search_suggestions, name='search_suggestions'),
-
-
 ]
